eae22f667e29d88daae2d47d4bc26588620e5ef3
Jimmy Chuk <jimmychuk@outlook.com>
2021-11-15 16:59:02 -0500

    added modal checkout ui

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react/cjs/react.production.min";
+import ReactDom from 'react-dom'
+import classes from "./Modal.module.css";
+
+const BackDrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays')
+
+
+const Modal = (props) => {
+
+    return (
+        <Fragment>
+            {ReactDom.createPortal(<BackDrop/>, portalElement)}
+            {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+
+        </Fragment>
+    )
+};
+
+export default Modal;
