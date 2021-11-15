e02f132f23a9b871b7d10723bdab4d8aac9904b2
Jimmy Chuk <jimmychuk@outlook.com>
2021-11-15 16:15:02 -0500

    added menu component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () =>{
+    return <section className={classes.summary}>
+        <h2>Delicious Food, Delivered To you</h2>
+        <p>
+            Choose your favourite meal from our broad selction of available meals andd enjoy a delicious lunch or dinner at home.
+        </p>
+        <p>
+            all our meals are cooled with high-quality ingredients, just-in-time andof course experienced chefs!
+        </p>
+    </section>
+}
+
+export default MealsSummary
